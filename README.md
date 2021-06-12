2f4928e8cd305e823da8437d1daca7c01ab56922
SNEHAMRAJPUROHIT <78412014+SNEHAMRAJPUROHIT@users.noreply.github.com>
2021-06-12 15:25:10 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# carracinggame3
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,31 @@
+var gameState=0;
+var playerCount=0 ;
+var database
+var game,player,form;
+var allPlayers;
+var distance=0;
+
+
+function setup(){
+   database=firebase.database()
+    createCanvas(500,500);
+    game=new Game()
+    game.getState()
+    game.start()
+    
+    
+}
+
+function draw(){
+    background("white");
+   if(playerCount==4){
+       game.update(1)
+    }
+if (gameState==1){
+    clear()
+    game.play()
+}
+    drawSprites();
+}
+
+
